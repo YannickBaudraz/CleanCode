abdb944af477726ca7b74c15c0bb2d305aa1ba7f
Yannick <yannickbaudrazdev@gmail.com>
2022-04-20 00:02:56 +0200

    Ideas and concept

---
+++ Functions and methods/Ideas and concept/bad.js
@@ -0,0 +1,18 @@
+function saveUser(email, password) {
+  const user = {
+    id: Math.random().toString(),
+    email: email,
+    password: password,
+  };
+
+  db.insert('users', user);
+}
+
+saveUser('test@test.com', 'testers');
+
+function saveUser(user) {
+  db.insert('users', user);
+}
+
+saveUser(newUser);
+
